5f69869be0f024fbcbfdd0829967ae9fa560d5f0
Sam Gammon <s@mg.sv>
2024-11-15 17:38:31 -0800

    fixup! `PushToken` alias import
    Signed-off-by: Sam Gammon <s@mg.sv>

--- crates/tauri-runtime-wry/src/lib.rs
+++ crates/tauri-runtime-wry/src/lib.rs
@@ -18,6 +18,7 @@ use http::Request;
 use raw_window_handle::{DisplayHandle, HasDisplayHandle, HasWindowHandle};
 
 use tauri_runtime::{
+  PushToken,
   dpi::{LogicalPosition, LogicalSize, PhysicalPosition, PhysicalSize, Position, Size},
   monitor::Monitor,
   webview::{DetachedWebview, DownloadEvent, PendingWebview, WebviewIpcHandler},
